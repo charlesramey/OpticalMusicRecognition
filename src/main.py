b0a9fb9919d2d22f8fb6b9227a271d270ccfdf6f
Sukris <sukrisvong@hotmail.com>
2019-10-28 17:43:30 -0400

    PEP8

--- src/main.py
+++ src/main.py
@@ -2,33 +2,34 @@ import matplotlib.pyplot as plt
 from StaffDetector import StaffDetector
 from myfunctions import get_input_im
 
-#Load image
+# Load image
 filename = 'transcription.jpg'
 input_im = get_input_im(filename)
 
-#Separate Staves
+# Separate Staves
 sd = StaffDetector(input_im)
 
+# Display Images
+plt.figure(dpi=200)
+plt.imshow(sd.im_original, cmap='gray')
 
-#Display Images
-plt.figure(dpi=500)
-plt.imshow(sd.im_original,cmap = 'gray')
+plt.figure(dpi=200)
+plt.imshow(sd.im_staves, cmap='Greys')
 
-plt.figure(dpi=500)
-plt.imshow(sd.im_staves,cmap = 'Greys')
+plt.figure(dpi=200)
+plt.imshow(sd.im_staves_expanded, cmap='Greys')
 
-plt.figure(dpi=500)
-plt.imshow(sd.im_staves_expanded,cmap = 'Greys')
-
-plt.figure(dpi=500)
-plt.imshow(sd.im_staves_filled,cmap = 'Greys')
-#plt.savefig('transcription_staff.jpg')
+plt.figure(dpi=200)
+plt.imshow(sd.im_staves_filled, cmap='Greys')
+# plt.savefig('transcription_staff.jpg')
 
 staves = sd.im_staves_separated
 
-plt.figure(dpi=500)
+plt.figure(dpi=200)
 for n in range(len(staves)):
     plt.subplot(len(staves),1,n+1)
     plt.imshow(staves[n],cmap = 'gray')
-    #plt.axis('off')
-#plt.savefig('transcription_staves.jpg')
+    # plt.axis('off')
+# plt.savefig('transcription_staves.jpg')
+
+plt.show()
--- src/myfunctions.py
+++ src/myfunctions.py
@@ -2,11 +2,12 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Returns row index of c-th detected gradient change
-def count_gradient_changes(im,c):
+
+# Returns row index of c-th detected gradient change
+def count_gradient_changes(im, c):
     count = 0
     previous_gradient = im[0][0]
-    for n in range(1,len(im)):
+    for n in range(1, len(im)):
         if im[n][0] == previous_gradient:
             continue
         previous_gradient = im[n][0]
@@ -16,25 +17,26 @@ def count_gradient_changes(im,c):
         
     return None
 
-#Loads and returns formatted input
+
+# Loads and returns formatted input
 def get_input_im(filename):
-    
+
     print("Getting input image...")
     
-    #Change directory to correct folder
+    # Change directory to correct folder
     directory = os.getcwd()
     directory = directory[:-4]
     os.chdir(directory + "\\inputs")
     
-    #Load image as np array
+    # Load image as np array
     im = np.array(plt.imread(filename))
 
-    #Get rid of third dimension of it exists
+    # Get rid of third dimension of it exists
     shape = np.shape(im)
     if len(shape) == 3:
-        im = im[:,:,0]
+        im = im[:, :, 0]
     
-    #Move directory back to original folder
+    # Move directory back to original folder
     os.chdir(directory)
     
     return im
